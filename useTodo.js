695b8c9e8df5a70dd3cb85eee570d66f53c6135a
Steve Carvajal <javier_zam1998@live.com>
2024-01-07 11:34:28 -0500

    Primera carga

---
+++ useTodo.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+  
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));    
+    }, [todos]);
+      
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatchTodo(action);
+    }
+
+    const handleDeteleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }  
+
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+        
+  
+    return {
+        ...todos,
+        todos,
+        handleNewTodo,
+        handleDeteleTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length
+    }
+}
